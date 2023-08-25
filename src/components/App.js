a11c9b4f89a1d446f84e41c33b2a3f90775f7bfe
Shivansh Raghav <shivanshraghav7@gmail.com>
2023-08-25 22:41:26 +0000

    sdssds

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,44 @@
 
-import React from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  let[search,Setsearch]=useState('')
+  let [arr,Setarr]=useState([])
+  function addata(){
+    if(search){
+      Setarr([...arr,search])
+      Setsearch('')
+    }
+  }
+  function deletefn(data){
+    let temparr=[...arr]
+    for(let i=0;i<arr.length;i++){
+      if(arr[i]==data){
+        temparr.splice(i,1)
+      }
+    }
+    Setarr(temparr)
+  }
   return (
     <div>
-        {/* Do not remove the main div */}
+       <div>
+        <h1>To-Do-List</h1>
+        <input type='text' value={search} onChange={(event)=>Setsearch(event.target.value)}></input>
+        <button onClick={addata}>Add-To-Do</button>
+       </div>
+       <div>
+        {
+          arr.map(element=>{
+            return (
+              <div>
+                <p>{element}</p>
+                <button onClick={()=>deletefn(element)}>Delete</button>
+              </div>
+            )
+          })
+        }
+       </div>
     </div>
   )
 }
